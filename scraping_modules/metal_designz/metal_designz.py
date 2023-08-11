68a78fb6d7f595dbddd5edc2f30e530951cbe648
Dakota McKay <dakotamckay72@gmail.com>
2023-08-11 17:38:41 -0400

    Added page url to metal designz  product parsing output.

--- scraping_modules/metal_designz/metal_designz.py
+++ scraping_modules/metal_designz/metal_designz.py
@@ -116,6 +116,7 @@ def parse_page(url: str, driver: webdriver) -> dict:
     # Define output dictionary
     out = {
         "time_accessed":       str(datetime.datetime.now()),
+        "url":                  url,
         "sku":                  None,
         "product_name":         None,
         "material":             None,
